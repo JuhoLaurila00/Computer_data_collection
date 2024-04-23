c48f79348bcbe8ddf9080741612d2f9d42855401
JuhoLaurila00 <juho.laurila00@gmail.com>
2024-04-23 19:00:16 +0300

    Selectable data, charts changed to plotly charts

--- filter.py
+++ filter.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import pandas
-import matplotlib.pyplot as plt
 import datetime
 import numpy
+import plotly.graph_objects as go
+import glob
 
 start_time = datetime.datetime(2024,4,22,18,47).timestamp()
 end_time = datetime.datetime(2024,4,22,19,0).timestamp()
@@ -10,20 +11,36 @@ time_step = 5
 rolling_average = 1
 time_range = numpy.linspace(start_time,end_time, num=int((end_time-start_time)/time_step))
 
-# fig, ax = plt.subplots()
-data = pandas.read_csv('test_cpu.csv')
-data['ccd_temp'] = data['ccd_temp'].rolling(window=rolling_average).mean()
-interp_data = numpy.interp(time_range, data['time'], data['ccd_temp'])
-time_range = pandas.to_datetime(time_range, unit='s')
-interp_data = numpy.nan_to_num(interp_data)
+dataframes = []
+for file in glob.iglob('*.csv'):
+    dataframes.append(pandas.read_csv(file))
 
-# ax.plot(time_range, interp_data, label='unfiltered', linewidth=1)
-# plt.ylim([0,100])
-# plt.xlim([time_range[0],time_range[-1]])
-# plt.legend()
-# plt.grid('Both')
-# plt.grid(which='minor', linewidth=0.4, linestyle='--')
-# plt.minorticks_on()
-# plt.savefig('filt.png', dpi=400)
 
+options = st.container(border=True)
+option_cols = options.columns(len(dataframes))
+checks = pandas.DataFrame(columns=['dataframe_id', 'device', 'col_name', 'enabled'])
+device_id = {0:'CPU', 1:'GPU'}
+for index, (df, col) in enumerate(zip(dataframes, option_cols)):
+    for header in df.columns:
+        if header != 'time':
+            check = col.checkbox(f'{device_id[index]} - {header}', key=f'{device_id[index]}{header}')
+            checks.loc[len(checks)] = [index, device_id[index], header, check]
+
+fig = go.Figure()
+for index, row in checks.loc[checks['enabled']==True].iterrows():
+    data = dataframes[checks.loc[index, 'dataframe_id']]
+    col = checks.loc[index, 'col_name']
+    dev = checks.loc[index, 'device']
+    interp_data = numpy.interp(time_range, data['time'], data[col])
+    interp_data = numpy.nan_to_num(interp_data)
+    fig.add_trace(go.Scatter(x=time_range, y=interp_data, mode='lines', name=f'{dev}-{col}'))
+yrange = [0,100]
 st.title('Data')
+
+fig.update_layout(
+    xaxis=dict(range=[time_range[0],time_range[-1]], showgrid=True),
+    yaxis=dict(range=yrange, showgrid=True, dtick=10, minor=dict(dtick=5, showgrid=True, griddash='3,2,3,2')),
+    showlegend=True,
+    legend=dict(y=-.2, orientation="h")
+    )
+st.plotly_chart(fig, use_container_width=True)
